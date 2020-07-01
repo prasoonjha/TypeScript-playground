bc695ed0ea68c52526866f0d986fbf55bb75c9aa
Prasoon Jha <prasoonjha69@gmail.com>
2020-07-01 20:45:55 +0530

    added User, Company and CustomMap classes

---
+++ maps/src/index.ts
@@ -0,0 +1,8 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+
+new CustomMap("map");
